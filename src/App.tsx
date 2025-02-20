54a9f6aa503ba01be83fdb4289adc00ab3cfe1b8
Rabiul Hasan Rafee <rhr277@gmail.com>
2025-02-20 21:26:30 +0600

    Header created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-
+import { useEffect, useState } from "react";
+import Navbar from "@/Components/Header/Navbar";
+import { SelectedPage } from "@/Shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/Shared/ActionButton";
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <>
-
-
-    </>
-  )
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      {/* <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer /> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
